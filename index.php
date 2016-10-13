2ea74b9b6193e00f38f3be6bace67f82f751cd20
aktailak <aktailak01@gmail.com>
2016-10-13 09:28:46 +0600

    Update and rename .gitignore to index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Clock</title>
+<?php require 'logic.php' ?>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body class="<?php echo $time_of_day ?>">
+<h1>It is <?php echo date('g:i a') ?></h1>
+<img src='http://making-the-internet.s3.amazonaws.com/php-<?php echo $time_of_day ?>.png'>
+</body>
+</html>
